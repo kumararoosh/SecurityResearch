a157742680c6403ed26be85a1584071cc1097b30
Aroosh Kumar <arkumar@cs.washington.edu>
2022-02-25 16:16:21 -0800

    Added Experiment for overlapping objects

--- SecurityResearch/ViewController.swift
+++ SecurityResearch/ViewController.swift
@@ -6,19 +6,43 @@
 //
 
 import UIKit
+import ARKit
 import RealityKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = arView.center
+        let results: [CollisionCastHit] = arView.hitTest(touch)
+        
+        let mesh01 = MeshResource.generateBox(size: 0.3)
+        let redBox = ModelEntity(mesh: mesh01, materials: [SimpleMaterial(color: .red, isMetallic: false)])
+        redBox.generateCollisionShapes(recursive: true)
+        redBox.name = "Red Cube"
+                
+        let yellowBox = ModelEntity(mesh: mesh01, materials: [SimpleMaterial(color: .yellow, isMetallic: false)])
+        yellowBox.generateCollisionShapes(recursive: true)
+        yellowBox.name = "Yellow Cube"
+       
+        if let result: CollisionCastHit = results.first {
+            print(result.entity.name)
+        } else {
+            if let raycastResult: ARRaycastResult = arView.raycast(from: arView.center, allowing: .estimatedPlane, alignment: .horizontal).last {
+                
+                let planeAnchor: AnchorEntity = AnchorEntity(raycastResult: raycastResult)
+                redBox.setParent(planeAnchor)
+                yellowBox.setParent(planeAnchor)
+                arView.scene.addAnchor(planeAnchor)
+            }
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
     }
 }
