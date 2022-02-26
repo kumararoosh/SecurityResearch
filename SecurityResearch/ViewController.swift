9215c8f1582671faa1d1c6c6170901e289cb727b
Aroosh Kumar <arkumar@cs.washington.edu>
2022-02-25 16:08:22 -0800

    Cleaned up code for displaying the visible cube within the invisible cube

--- SecurityResearch/ViewController.swift
+++ SecurityResearch/ViewController.swift
@@ -18,22 +18,26 @@ class ViewController: UIViewController {
         let touch = arView.center
         let results: [CollisionCastHit] = arView.hitTest(touch)
         
-        let mesh02 = MeshResource.generateBox(size: 0.3)
-        let box1 = ModelEntity(mesh: mesh02, materials: [SimpleMaterial(color: .red, isMetallic: false)])
-        box1.generateCollisionShapes(recursive: true)
-        box1.name = "Visible Cube"
+        let mesh01 = MeshResource.generateBox(size: 0.3)
+        let box = ModelEntity(mesh: mesh01, materials: [SimpleMaterial()])
+        box.generateCollisionShapes(recursive: true)
+        box.name = "Visible Cube"
         
-        let box2 = ModelEntity(mesh: mesh02, materials: [SimpleMaterial(color: .yellow, isMetallic: false)])
+        let mesh02 = MeshResource.generateBox(size: 0.5)
+        let invisibleMaterial = SimpleMaterial(color: SimpleMaterial.Color(hue: 1, saturation: 1, brightness: 1, alpha: 0), roughness: .float(0.2), isMetallic: false)
+        
+        let invisibleBox = ModelEntity(mesh: mesh02, materials: [invisibleMaterial])
+        invisibleBox.generateCollisionShapes(recursive: true)
+        invisibleBox.name = "Invisible Box"
        
         if let result: CollisionCastHit = results.first {
-            
             print(result.entity.name)
         } else {
-            if let raycastResult: ARRaycastResult = arView.raycast(from: arView.center, allowing: .existingPlaneInfinite, alignment: .horizontal).last {
+            if let raycastResult: ARRaycastResult = arView.raycast(from: arView.center, allowing: .estimatedPlane, alignment: .horizontal).last {
                 
                 let planeAnchor: AnchorEntity = AnchorEntity(raycastResult: raycastResult)
-                box1.setParent(planeAnchor)
-                box2.setParent(planeAnchor)
+                box.setParent(planeAnchor)
+                invisibleBox.setParent(planeAnchor)
                 arView.scene.addAnchor(planeAnchor)
             }
         }
@@ -46,24 +50,6 @@ class ViewController: UIViewController {
         
         arView.renderOptions.insert(.disableAREnvironmentLighting)
 
-        // Model for collision
-        let mesh02 = MeshResource.generateBox(size: 0.3)
-        let box = ModelEntity(mesh: mesh02, materials: [SimpleMaterial()])
-        box.generateCollisionShapes(recursive: true)
-        box.name = "Visible Cube"
-        let planeAnchor = AnchorEntity(.plane(.any,
-                              classification: .any,
-                               minimumBounds: [0.2, 0.2]))
-        box.setParent(planeAnchor)
-        
-        let mesh03 = MeshResource.generateBox(size: 0.5)
-        let invisibleMaterial = SimpleMaterial(color: SimpleMaterial.Color(hue: 1, saturation: 1, brightness: 1, alpha: 0), roughness: MaterialScalarParameter.float(0.2), isMetallic: false)
-        
-        let invisibleBox = ModelEntity(mesh: mesh03, materials: [invisibleMaterial])
-        invisibleBox.generateCollisionShapes(recursive: true)
-        invisibleBox.name = "Invisible Box"
-        invisibleBox.setParent(planeAnchor)
-        arView.scene.addAnchor(planeAnchor)
     }
 }
 
