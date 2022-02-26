a61315e626227022c60315145608ae2e90189bc2
Aroosh Kumar <arkumar@cs.washington.edu>
2022-02-25 16:23:35 -0800

    Added starter code to place cubes on plane that raycast intersects

--- SecurityResearch/ViewController.swift
+++ SecurityResearch/ViewController.swift
@@ -6,19 +6,38 @@
 //
 
 import UIKit
+import ARKit
 import RealityKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = arView.center
+        let results: [CollisionCastHit] = arView.hitTest(touch)
+        
+        let mesh01 = MeshResource.generateBox(size: 0.3)
+        let box = ModelEntity(mesh: mesh01, materials: [SimpleMaterial(color: .red, isMetallic: false)])
+        box.generateCollisionShapes(recursive: true)
+        box.name = "Box"
+       
+        if let result: CollisionCastHit = results.first {
+            print(result.entity.name)
+        } else {
+            if let raycastResult: ARRaycastResult = arView.raycast(from: arView.center, allowing: .estimatedPlane, alignment: .horizontal).last {
+                
+                let planeAnchor: AnchorEntity = AnchorEntity(raycastResult: raycastResult)
+                box.setParent(planeAnchor)
+                arView.scene.addAnchor(planeAnchor)
+            }
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+
     }
 }
