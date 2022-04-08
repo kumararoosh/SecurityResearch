3195db3fbbe390bb924ed593548020214c4b9e20
Aroosh Kumar <kumararoosh@gmail.com>
2022-04-08 14:14:35 -0700

    Finished a man in the middle attack

--- SecurityResearch/ViewController.swift
+++ SecurityResearch/ViewController.swift
@@ -21,15 +21,24 @@ class ViewController: UIViewController {
         let box = ModelEntity(mesh: mesh01, materials: [SimpleMaterial(color: .red, isMetallic: false)])
         box.generateCollisionShapes(recursive: true)
         box.name = "Box"
-       
+        
+        let mesh02 = MeshResource.generateBox(size: 0.5)
+        let box2 = ModelEntity(mesh: mesh02, materials: [SimpleMaterial(color: .clear, isMetallic: true)])
+        box2.generateCollisionShapes(recursive: true)
+        box2.name = "malicious"
+    
+        
         if let result: CollisionCastHit = results.first {
-            print(result.entity.name)
+            result.entity.onClick(arView: arView)
+            
         } else {
             if let raycastResult: ARRaycastResult = arView.raycast(from: arView.center, allowing: .estimatedPlane, alignment: .horizontal).last {
                 
                 let planeAnchor: AnchorEntity = AnchorEntity(raycastResult: raycastResult)
                 box.setParent(planeAnchor)
+                box2.setParent(planeAnchor)
                 arView.scene.addAnchor(planeAnchor)
+                
             }
         }
         
@@ -39,5 +48,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+}
+
+
+extension Entity {
+
+    func onClick(arView: ARView) {
+        if (self.name == "malicious") {
+            print("captured input")
+            let direction: SIMD3<Float> =  self.position(relativeTo: nil) - arView.cameraTransform.translation
+            let worldRayCast: [CollisionCastHit] = arView.scene.raycast(origin: arView.cameraTransform.translation, direction: direction, length: 1000, query: .all, mask: .all, relativeTo: nil)
+            print(worldRayCast.count)
+            if (worldRayCast.count > 1) {
+                print(worldRayCast[1])
+                worldRayCast[1].entity.onClick(arView: arView)
+            }
+        } else {
+            print(self.name)
+        }
     }
 }
